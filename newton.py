70f23bc10ffe05cc83cd5016d29fdb17822f0b70
Erin Nell <your_name@berkeley.edu>
2026-08-19 14:15:34 -0700

    formatting changes without mistake

--- newton.py
+++ newton.py
@@ -1,33 +1,40 @@
 def derivative(f, x, h=1e-5):
-    return (f(x + h) - f(x))/h
+    return (f(x + h) - f(x)) / h
 
-#def f(x):
-    #return x**2
-    
-#print(derivative(f, 3))
+
+# def f(x):
+# return x**2
+
+# print(derivative(f, 3))
+
 
 def derivative2(f, x, h=(1e-5)):
-    return derivative(lambda z: derivative(f,z,h), x, h)
+    return derivative(lambda z: derivative(f, z, h), x, h)
 
-#print(derivative2(f, 3)
 
-def newton(f, x0, tolerance = 1e-5, iterations = 100):
+# print(derivative2(f, 3)
+
+
+def newton(f, x0, tolerance=1e-5, iterations=100):
+    """doc string"""
     x = x0
+
     for i in range(iterations):
-        first = derivative(f , x)
-        second = derivative2(f ,x)
+        first = derivative(f, x)
+        second = derivative2(f, x)
 
         if abs(first) < tolerance:
             break
-            
-        x = x - first/second
+
+        x = x - first / second
 
     return x
 
+
 def f(x):
-    return x**2
+    return x**3
 
 
-min = newton(f, x0=0)
+min = newton(f, x0 = 0)
 print(min)
 print(f(min))
